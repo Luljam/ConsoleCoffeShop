320af84e1575d448e13e096b3375309983702147
Luciano Brito <42751775+Luljam@users.noreply.github.com>
2023-08-10 14:51:13 -0300

    Add coffee shop Lyon

--- ConsoleCoffeShop/Data/CoffeeShopDataPrivider.cs
+++ ConsoleCoffeShop/Data/CoffeeShopDataPrivider.cs
@@ -10,7 +10,8 @@ public class CoffeeShopDataPrivider
         {
             new CoffeeShop("Franckfurt", 45),
             new CoffeeShop("Zurich", 39),
-            new CoffeeShop("Rome", 25)
+            new CoffeeShop("Rome", 25),
+            new CoffeeShop("Lyon", 57)
         };
     }
 }
