5ae65a54bb1dc65490b5bb1cfeb73566253cc92f
luciano.f.de.brito <luciano.f.de.brito@avanade.com>
2023-08-10 14:44:40 -0300

    Add coffee shop data provider

---
+++ ConsoleCoffeShop/Data/CoffeeShopDataPrivider.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleCoffeShop.Model;
+
+namespace ConsoleCoffeShop.Data;
+
+public class CoffeeShopDataPrivider
+{
+    public IEnumerable<CoffeeShop> LoadShops()
+    {
+        return new CoffeeShop[]
+        {
+            new CoffeeShop("Franckfurt", 45),
+            new CoffeeShop("Zurich", 39),
+            new CoffeeShop("Rome", 25)
+        };
+    }
+}
