b650685d2b914555a0a9a2bb41c5dbc00a644af6
luciano.f.de.brito <luciano.f.de.brito@avanade.com>
2023-08-10 15:30:11 -0300

    Implement shops command

--- ConsoleCoffeShop/Program.cs
+++ ConsoleCoffeShop/Program.cs
@@ -1,8 +1,11 @@
-﻿Console.WriteLine("-------------------------------");
+﻿using ConsoleCoffeShop.Data;
+
+Console.WriteLine("-------------------------------");
 Console.WriteLine(" Wired Coffee Shop - Info Tool ");
 Console.WriteLine("-------------------------------");
 Console.WriteLine("Commands:");
 Console.WriteLine(" quit  exit application");
+Console.WriteLine(" shops  show coffeshops");
 
 while (true)
 {
@@ -11,4 +14,15 @@ while (true)
     {
         break;
     }
+    else if (string.Equals("shops", command, StringComparison.OrdinalIgnoreCase))
+    {
+        var dataProvider = new CoffeeShopDataProvider();
+        var coffeeShops = dataProvider.LoadShops();
+
+        foreach (var coffeeShop in coffeeShops)
+        {
+            Console.WriteLine(coffeeShop);
+        }
+    }
+
 }
